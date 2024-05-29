b34d00c5cd86b9ca8511d6b9671fe76cb7fe13e4
vrendonestrada@gmail.com <vrendonestrada@gmail.com>
2024-05-28 20:33:48 -0500

    las rutas de los thunder client funcionan con la base de datos conectada

--- app/controllers/controllers.marca.js
+++ app/controllers/controllers.marca.js
@@ -1,22 +1,74 @@
 import { pool } from "../config/db.mysql.js";
 
-export const mostrarMarca = (req,res) => {
-    console.log("funciona");
+export const mostrarMarca = async(req,res) => {
+    let id =req.params['id'];
+
+    try {
+        const respuesta = await pool.query(`CALL sp_MostrarMarca(${id});`);
+        res.json({"res": respuesta});
+    } catch (err) {
+        res.json({"erro": err})
+        
+    }
 }
 
-export const listarMarca = (req,res) => {
-    console.log("funciona");
+export const listarMarca = async(req,res) => {
+
+    try {
+        const respuesta = await pool.query(`CALL sp_ListarMarca`);
+        res.json({"res": respuesta});
+    } catch (err) {
+        res.json({"erro": err})
+        
+    }
 }
 
-export const crearMarca = (req,res) => {
-    console.log("funciona");
+export const crearMarca = async(req,res) => {
+    const descripcion = req.body.descripcion;
+    try {
+        const respuesta = await pool.query(`CALL sp_CrearMarca('${descripcion}');`);
+        if(respuesta[0].affectedRows == 1){
+            res.json({"res": "Marca Creada"});
+        }else{
+            res.json({"error": "No se creo la Marca"});
+        }
+        
+    } catch (err) {
+        res.json({"erro": err})
+        
+    }
 }
 
-export const modificarMarca = (req,res) => {
-    console.log("funciona");
+export const modificarMarca = async(req,res) => {
+    let id = req.body.id;
+    let descripcion = req.body.descripcion;
+    try {
+        const respuesta = await pool.query(`CALL sp_ModificarMarca(${id},'${descripcion}');`);
+        if(respuesta[0].affectedRows == 1){
+            res.json({"res": "Marca modificada"});
+        }else{
+            res.json({"error": "No se modifico la Marca"});
+        }
+        
+    } catch (err) {
+        res.json({"erro": err})
+        
+    }
 }
 
-export const eliminarMarca = (req,res) => {
-    console.log("funciona");
+export const eliminarMarca = async(req,res) => {
+    let id = req.body.id;
+    try {
+        const respuesta = await pool.query(`CALL sp_EliminarMarca(${id});`);
+        if(respuesta[0].affectedRows == 1){
+            res.json({"res": "Marca eliminada"});
+        }else{
+            res.json({"error": "No se elimino la Marca"});
+        }
+        
+    } catch (err) {
+        res.json({"erro": err})
+        
+    }
 }
 
